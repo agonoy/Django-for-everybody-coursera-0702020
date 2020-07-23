c3fb403c63469805801579fe1f90d6ddca84ed3c
agonoy <kenshen929@icloud.com>
2020-07-22 16:30:22 -1000

    web client

--- web_client.py
+++ web_client.py
@@ -1,16 +1,12 @@
+# simple web client
 import socket
   
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-
-# send to request .. .encode()
-cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
-
-#  talk first. Thus .send(cmd)
+mysock.connect(('127.0.0.1', 9000))
+cmd = 'GET http://127.0.0.1/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
-    # 512 characters
     data = mysock.recv(512)
     if len(data) < 1:
         break
